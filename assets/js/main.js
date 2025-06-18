85f15221ad661aa8fea75684bf1ce5f5e32f8f44
caiorobotics <caiorobotics@gmail.com>
2025-06-18 15:10:02 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -297,11 +297,11 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1281px',   '1680px' ],
-			large:    [ '981px',    '1280px' ],
-			medium:   [ '737px',    '980px'  ],
-			small:    [ '481px',    '736px'  ],
-			xsmall:   [ null,       '480px'  ]
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ]
 		});
 
 	// Hack: Enable IE workarounds.
@@ -498,67 +498,63 @@
 			$main.children('.thumb').each(function() {
 
 				var	$this = $(this),
-					$image = $this.find('.image'), $image_img = $image.children('img'),
+					$image = $this.find('.image'),
+					// Changed to find video OR image for thumbnail setting
+					$mediaElement = $image.children('img, video'),
 					x;
 
-				// No image? Bail.
-					if ($image.length == 0)
+				// No media element? Bail.
+					if ($mediaElement.length == 0)
 						return;
 
-				// Image.
-				// This sets the background of the "image" <span> to the image pointed to by its child
-				// <img> (which is then hidden). Gives us way more flexibility.
-
+				// Media.
+				// This sets the background of the "image" <span> to the thumbnail if it's an image.
+                // For videos, we rely on the <video> element itself.
+				if ($mediaElement.is('img')) { // Only apply background-image to actual images
 					// Set background.
-						$image.css('background-image', 'url(' + $image_img.attr('src') + ')');
+					$image.css('background-image', 'url(' + $mediaElement.attr('src') + ')');
 
 					// Set background position.
-						if (x = $image_img.data('position'))
-							$image.css('background-position', x);
+					if (x = $mediaElement.data('position'))
+						$image.css('background-position', x);
 
 					// Hide original img.
-						$image_img.hide();
+					$mediaElement.hide();
+				}
 
 			});
 
-		// Poptrox.
+		// Poptrox (for images only).
 			$main.poptrox({
 				baseZIndex: 20000,
 				caption: function($a) {
+
 					var s = '';
+
 					$a.nextAll().each(function() {
 						s += this.outerHTML;
 					});
+
 					return s;
+
 				},
 				fadeSpeed: 300,
 				onPopupClose: function() { $body.removeClass('modal-active'); },
 				onPopupOpen: function() { $body.addClass('modal-active'); },
-				overlayOpacity: 0,
+				overlayOpacity: 0.75, // Changed from 0 for better visibility of popup
 				popupCloserText: '',
-				popupHeight: 480, // Adjusted for typical 16:9 video
+				popupHeight: 150,
 				popupLoaderText: '',
 				popupSpeed: 300,
-				popupWidth: 854, // Adjusted for typical 16:9 video
-				selector: '.thumb > a.image', // This selector is already targeting the correct elements
+				popupWidth: 150,
+				selector: '.thumb > a.image:not(.video-link)', // <--- Modified selector to exclude video links
 				usePopupCaption: true,
 				usePopupCloser: true,
 				usePopupDefaultStyling: false,
 				usePopupForceClose: true,
 				usePopupLoader: true,
 				usePopupNav: true,
-				windowMargin: 50,
-
-				// --- NEW: Add popupContent to handle videos ---
-				popupContent: function(url) {
-					// Check if the URL is a video (you can extend this with more video formats)
-					if (url.match(/\.(mp4|webm|ogg)$/i)) {
-						return '<video controls autoplay loop muted playsinline src="' + url + '" style="width: 100%; height: 100%; object-fit: contain;"></video>';
-					}
-					// If it's not a video, let Poptrox handle it as an image or default
-					return '<img src="' + url + '" alt="" />';
-				}
-				// --- END NEW ---
+				windowMargin: 50
 			});
 
 			// Hack: Set margins to 0 when 'xsmall' activates.
@@ -569,5 +565,82 @@
 				breakpoints.on('>xsmall', function() {
 					$main[0]._poptrox.windowMargin = 50;
 				});
+
+    // Custom video modal handling
+    $('.video-link').on('click', function(e) {
+        e.preventDefault(); // Prevent poptrox or default link behavior
+
+        var videoSrc = $(this).attr('href');
+        var $videoElement = $('<video controls autoplay loop muted playsinline></video>');
+        $videoElement.attr('src', videoSrc);
+
+        var $videoModal = $(`
+            <div id="video-modal" style="
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: rgba(0, 0, 0, 0.85);
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                z-index: 20001; /* Higher than poptrox baseZIndex */
+            ">
+                <div style="
+                    max-width: 90%;
+                    max-height: 90%;
+                    position: relative;
+                ">
+                    <span class="close-video" style="
+                        position: absolute;
+                        top: 10px;
+                        right: 10px;
+                        color: #fff;
+                        font-size: 2em;
+                        cursor: pointer;
+                        z-index: 20002;
+                        font-family: sans-serif; /* Ensure a standard font for 'X' */
+                    ">&times;</span>
+                </div>
+            </div>
+        `);
+
+        $videoModal.find('div').prepend($videoElement); // Add video to the inner div
+        $body.append($videoModal);
+
+        // Resize video to fit within the modal
+        $videoElement.css({
+            'max-width': '100%',
+            'max-height': '100%',
+            'width': 'auto',
+            'height': 'auto'
+        });
+
+        // Event listener for closing the modal
+        $videoModal.on('click', '.close-video', function() {
+            $videoModal.remove();
+            // Pause the video when closing
+            $videoElement[0].pause();
+        });
+
+        // Close modal on escape key
+        $window.on('keydown.videoModal', function(e) {
+            if (e.keyCode === 27) { // 27 is the keycode for ESC
+                $videoModal.remove();
+                $videoElement[0].pause();
+                $window.off('keydown.videoModal'); // Remove the event listener
+            }
+        });
+
+        // Handle clicks outside the video to close the modal
+        $videoModal.on('click', function(e) {
+            if ($(e.target).is($videoModal)) {
+                $videoModal.remove();
+                $videoElement[0].pause();
+            }
+        });
+    });
+
 
 })(jQuery);
